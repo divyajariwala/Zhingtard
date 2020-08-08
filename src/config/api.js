b6490e9fd15658d71d0ac77105782086eda2ff7e
MyPapion <divyajariwala610@gmail.com>
2020-08-07 23:52:29 -0700

    commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://bd.zhingtard.com';
+
+export const CONSUMER_KEY = 'ck_379ae9e862cc6f4212f45d03f80e79cee9357071';
+export const CONSUMER_SECRET = 'cs_bd723a299c58d6537a6292222ca0225e0185ba88';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET
+};
---
+++ src/config/onesignal.js
@@ -0,0 +1,1 @@
+export const APP_ID = 'b850b32c-b9bc-453b-a3a8-bce88460a639';
